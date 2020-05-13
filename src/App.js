17654ec09a9bf9db0b5278de65bfb34d4541df10
Bilge Kaan KAZAZ <bkkazaz@kazaz.com.tr>
2020-05-13 17:52:09 +0300

    Trial 3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React from 'react';
 
-function App() {
-  const greeting = {
-    name: "Kaan",
-    surname:"KAZAZ"
-  };
-  
-  function getSeconMessageFromFunction()
+const movies = [
   {
-    return 'And this is the second message...';
+    id: 1,
+    title: "The Godfather",
+    imdbScore: 9.2,
+    url: "https://www.imdb.com/title/tt0068646/"
+  },
+  {
+    id: 2,
+    title: "The Godfather: Part II",
+    imdbScore: 9.0,
+    url: "https://www.imdb.com/title/tt0071562/"
+  },
+  {
+    id: 3,
+    title: "The Godfather: Part III",
+    imdbScore: 7.6,
+    url: "https://www.imdb.com/title/tt0099674/"
   }
-  
-  
+]
+
+function App() {
   return (
     <div>
-      <h1>Hello {greeting.name} {greeting.surname}</h1>
-      <h2>{getSeconMessageFromFunction()}</h2>
+      <h1>Movies</h1>
+      <hr />
+      {
+        movies.map(function (movie) {
+          return (
+            <div key={movie.id}>
+              <a href={movie.url}>{movie.title} - {movie.imdbScore}</a>
+              </div>
+          )
+        })
+      }
     </div>
   );
 }
