c134b69c08c7b2a375079113eb36d6a98d4c7e98
Bilge Kaan KAZAZ <bkkazaz@kazaz.com.tr>
2020-05-16 16:29:57 +0300

    Arrow functions and object destructing

--- src/DisplayMovies.js
+++ src/DisplayMovies.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-function DisplayMovies(props)
-{
-  return props.movies.map(function (movie) {
-    return (
-      <div key={movie.id}>
-        <a href={movie.url}>{movie.title} - {movie.imdbScore}</a>
-        </div>
-    )
-  })
+// const DisplayMovies = (props) =>
+const DisplayMovies = ({ movies }) => {
+    //   return props.movies.map(function (movie) {
+    return movies.map(movie => {
+        return (
+            <div key={movie.id}>
+                <a href={movie.url}>{movie.title} - {movie.imdbScore}</a>
+            </div>
+        );
+    });
 }
 
 export default DisplayMovies;
