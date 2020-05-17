4bfe3701f6063281470268ddbbce172f2d5cc2c5
Kaan KAZAZ <kaan.kazaz@gmail.com>
2020-05-17 13:43:24 +0300

    test commit on ubuntu

--- src/DisplayMovies.js
+++ src/DisplayMovies.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // const DisplayMovies = (props) =>
 const DisplayMovies = ({ movies }) => {
-    //   return props.movies.map(function (movie) {
+    // return props.movies.map(function (movie) {
     return movies.map(movie => {
         return (
             <div key={movie.id}>
